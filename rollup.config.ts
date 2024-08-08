4fd1e3254825c3a4b45526913e3d97e28ae28ead
Julia K <korchkova.yulia@gmail.com>
2024-08-08 19:38:59 +0300

    build: added rollup config and babel
      - Updated tsconfig to remove the following options: sourceMap,
    allowImportingTsExtensions, declaration, emitDeclarationOnly,
    incremental. The following properties have been removed for rollup
    integration
      - Added new packages:
        - Babel: babel/core, babel/plugin-transform-runtime,
      babel/preset-env, babel/preset-react, babel/preset-typescript
        - Rollup: rollup/plugin-babel, rollup/plugin-commonjs,
      rollup/plugin-node-resolve, rollup/plugin-terser,
      rollup/plugin-typescript, types/rollup-plugin-peer-deps-external,
      rollup, rollup-plugin-dts, rollup-plugin-peer-deps-external, tslib
      - Updated package.json to include the following scripts: clean,
    clean:npm, dev, build
      - Updated peerDependencies to use 18 versions
      - Added the following properties to package.json: type, module,
    types for rollup config output
      - Added babel configuration and rollup configuration

---
+++ rollup.config.ts
@@ -0,0 +1,55 @@
+import { RollupOptions } from 'rollup';
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser'
+import { babel } from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { dts } from "rollup-plugin-dts";
+
+export default [
+	{
+		input: './src/index.ts',
+    preserveModules: true,
+		output: [
+      {
+        dir: 'build',
+        format: 'esm',
+        exports: 'named',
+        sourcemap: true,
+      },
+      {
+        file: 'build/index.cjs.js',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+		],
+		plugins: [
+			peerDepsExternal(),
+      nodeResolve(),
+      commonjs(),
+			typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+				declarationDir: 'build',
+				sourceMap: false,
+      }),
+      terser(),
+			babel({
+				configFile: './.babelrc',
+				babelHelpers: 'runtime',
+				exclude: 'node_modules/**',
+			}),
+		],
+		external: ['react', 'react-dom'],
+	},
+  {
+    input: "build/src/index.d.ts",
+    output: [{
+      file: "build/index.d.ts", format: "esm"
+    }],
+    plugins: [ dts() ],
+    external: [/\.css$/],
+  },
+] as RollupOptions;
