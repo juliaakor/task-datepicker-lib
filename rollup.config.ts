575f6f83ab97beb55b1c3c8bf3df92bf60a848eb
Julia K <korchkova.yulia@gmail.com>
2024-08-08 22:53:46 +0300

    style: apply prettier fixes

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,17 +1,17 @@
 import { RollupOptions } from 'rollup';
-import commonjs from "@rollup/plugin-commonjs";
+import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser'
+import terser from '@rollup/plugin-terser';
 import { babel } from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import { dts } from "rollup-plugin-dts";
+import { dts } from 'rollup-plugin-dts';
 
 export default [
-	{
-		input: './src/index.ts',
+  {
+    input: './src/index.ts',
     preserveModules: true,
-		output: [
+    output: [
       {
         dir: 'build',
         format: 'esm',
@@ -24,32 +24,35 @@ export default [
         exports: 'named',
         sourcemap: true,
       },
-		],
-		plugins: [
-			peerDepsExternal(),
+    ],
+    plugins: [
+      peerDepsExternal(),
       nodeResolve(),
       commonjs(),
-			typescript({
+      typescript({
         tsconfig: './tsconfig.json',
         declaration: true,
-				declarationDir: 'build',
-				sourceMap: false,
+        declarationDir: 'build',
+        sourceMap: false,
       }),
       terser(),
-			babel({
-				configFile: './.babelrc',
-				babelHelpers: 'runtime',
-				exclude: 'node_modules/**',
-			}),
-		],
-		external: ['react', 'react-dom'],
-	},
+      babel({
+        configFile: './.babelrc',
+        babelHelpers: 'runtime',
+        exclude: 'node_modules/**',
+      }),
+    ],
+    external: ['react', 'react-dom'],
+  },
   {
-    input: "build/src/index.d.ts",
-    output: [{
-      file: "build/index.d.ts", format: "esm"
-    }],
-    plugins: [ dts() ],
+    input: 'build/src/index.d.ts',
+    output: [
+      {
+        file: 'build/index.d.ts',
+        format: 'esm',
+      },
+    ],
+    plugins: [dts()],
     external: [/\.css$/],
   },
 ] as RollupOptions;
