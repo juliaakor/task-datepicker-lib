757a8967029edb447ca71a9e69a9c2f4df757d73
Julia K <korchkova.yulia@gmail.com>
2024-08-09 12:19:44 +0300

    feat: add stories example with empty component
    Added stories example for chromatic build  (will be updated in future,
    these component are manually for test)

---
+++ src/stories/Calendar.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import { Calendar } from '../components';
+
+const meta = {
+  args: { onClick: fn() },
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  component: Calendar,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  title: 'Datepicker/Calendar',
+} satisfies Meta<typeof Calendar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const CalendarView: Story = {
+  args: {
+    label: 'Calendar',
+  },
+};
