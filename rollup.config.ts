045b48958cd11c9968d5b2b9be68c78a632b54b2
Julia K <korchkova.yulia@gmail.com>
2024-08-09 12:17:27 +0300

    fix: update rollup config to include eslint rules

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,6 @@
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import eslint from '@rollup/plugin-eslint';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
@@ -39,6 +40,9 @@ export default [
       babel({
         babelHelpers: 'runtime',
         configFile: './.babelrc',
+        exclude: 'node_modules/**',
+      }),
+      eslint({
         exclude: 'node_modules/**',
       }),
     ],
