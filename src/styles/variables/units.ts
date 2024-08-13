e1bd4ca10f831addb584e42ca0e36727ae1412e5
Julia K <korchkova.yulia@gmail.com>
2024-08-13 12:39:41 +0300

    style: add styles variables
      - The following variables objects have been added: colors, fontWeight,
    fontSize, lineHeight, fontFamily, borderRadius, spacing, size,
    borderWidth
      - Added exports for variables

---
+++ src/styles/variables/units.ts
@@ -0,0 +1,28 @@
+export const borderRadius = {
+  large: 15,
+  medium: 8,
+  none: 0,
+  small: 3,
+};
+
+export const spacing = {
+  large: 15,
+  medium: 11,
+  small: 7,
+  xLarge: 13,
+  xMedium: 10,
+  xSmall: 6,
+  xxLarge: 12,
+  xxMedium: 8,
+  xxSmall: 5,
+};
+
+export const size = {
+  minCalerndarWidth: 250,
+};
+
+export const borderWidth = {
+  large: 4,
+  medium: 2,
+  thin: 1,
+};
