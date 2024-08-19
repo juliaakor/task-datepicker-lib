93f2b3693655ca8427cbe9c26423833c7b5e3d07
Julia K <korchkova.yulia@gmail.com>
2024-08-19 08:25:06 +0300

    test: update tests for decorators exports

--- src/components/decorators/__tests__/exports.test.ts
+++ src/components/decorators/__tests__/exports.test.ts
@@ -1,6 +1,7 @@
 import * as index from '@components/decorators';
 import { withCalendarView } from '@components/decorators/WithCalendarView';
 import { withCustomTheme } from '@components/decorators/WithCustomTheme';
+import { withWeekendManager } from '@components/decorators/WithWeekendManager';
 
 describe('components folder index file exports', () => {
   it('should export withCalendarView from index', () => {
@@ -9,5 +10,9 @@ describe('components folder index file exports', () => {
 
   it('should export withCustomTheme from index', () => {
     expect(index.withCustomTheme).toBe(withCustomTheme);
+  });
+
+  it('should export withWeekendManager from index', () => {
+    expect(index.withWeekendManager).toBe(withWeekendManager);
   });
 });
