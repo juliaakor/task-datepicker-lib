dffe593b03eb96d8ad856ef9d6eb97244dceae54
Julia K <korchkova.yulia@gmail.com>
2024-08-22 21:38:03 +0300

    feat: add deleteIcon and editIcon
      - Added deleteIcon and editIcon to the assets folder and updated index
    export file to include those icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,5 @@ export { CalendarIcon } from './calendarIcon';
 export { ClearIcon } from './clearIcon';
 export { NextIcon } from './nextIcon';
 export { PrevIcon } from './prevIcon';
+export { DeleteIcon } from './deleteIcon';
+export { EditIcon } from './editIcon';
